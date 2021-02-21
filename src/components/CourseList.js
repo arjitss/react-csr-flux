b61c037154998251c68aa5a5fed53364bdf60fc9
Arjit <arjit.sharma@gmail.com>
2021-02-21 16:07:17 +0000

    Seperated logic and presentation and added proptypes

---
+++ src/components/CourseList.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/** Two ways to display data in react
+ * 1. Calling a method on each map elemet - GetCourseData1
+ * 2. Calling a JSX component and passing data as props- GetCourseData2
+ */
+const GetCourseData1 = (course) => {
+  return (
+    <tr key={course.id}>
+      <td>{course.title}</td>
+      <td>{course.authorId}</td>
+      <td>{course.category}</td>
+    </tr>
+  );
+};
+
+const GetCourseData2 = (props) => {
+  console.log(props.courseArray.length);
+  if (props.courseArray.length > 0) {
+    return props.courseArray.map((course) => {
+      return (
+        <tr key={course.id}>
+          <td>{course.title}</td>
+          <td>{course.authorId}</td>
+          <td>{course.category}</td>
+        </tr>
+      );
+    });
+  } else {
+    return (
+      <tr>
+        <td>Waiting for data...!!!</td>
+      </tr>
+    );
+  }
+};
+
+function CourseList(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author Id</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <GetCourseData2 courseArray={props.courses} />
+      </tbody>
+      <tbody style={{ border: '5px solid #ccc', backgroundColor: 'pink' }}>
+        {props.courses.map((course) => {
+          return GetCourseData1(course);
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+CourseList.propTypes = {
+  courses: PropTypes.array.isRequired,
+};
+
+export default CourseList;
--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getCourses } from '../api/courseApi';
+import CourseList from './CourseList';
 
 CoursePage.propTypes = {};
 
@@ -16,60 +17,10 @@ function CoursePage(props) {
       });
   }, []);
 
-  /** Two ways to display data in react
-   * 1. Calling a method on each map elemet - GetCourseData1
-   * 2. Calling a JSX component and passing data as props- GetCourseData2
-   */
-  const GetCourseData1 = (course) => {
-    return (
-      <tr key={course.id}>
-        <td>{course.title}</td>
-        <td>{course.authorId}</td>
-        <td>{course.category}</td>
-      </tr>
-    );
-  };
-
-  const GetCourseData2 = (props) => {
-    console.log(props.courseArray.length);
-    if (props.courseArray.length > 0) {
-      return props.courseArray.map((course) => {
-        return (
-          <tr key={course.id}>
-            <td>{course.title}</td>
-            <td>{course.authorId}</td>
-            <td>{course.category}</td>
-          </tr>
-        );
-      });
-    } else {
-      return (
-        <tr>
-          <td>Waiting for data...!!!</td>
-        </tr>
-      );
-    }
-  };
   return (
     <div>
       <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author Id</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          <GetCourseData2 courseArray={coursesData.courses} />
-        </tbody>
-        <tbody style={{ border: '5px solid #ccc', backgroundColor: 'pink' }}>
-          {coursesData.courses.map((course) => {
-            return GetCourseData1(course);
-          })}
-        </tbody>
-      </table>
+      <CourseList courses={coursesData.courses} />
     </div>
   );
 }
