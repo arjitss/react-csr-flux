8c44dbe78c5bd6bda75a612772c4998acf9c5982
Arjit <arjit.sharma@gmail.com>
2021-02-21 21:44:28 +0000

    Adding prompt to alert while leaving page

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Prompt } from 'react-router-dom';
 
 ManageCoursePage.propTypes = {};
 
@@ -7,7 +8,9 @@ function ManageCoursePage(props) {
   return (
     <div>
       <h2>ManageCoursePage</h2>
+      <Prompt when={true} message="Are you sure ?" />
       {props.match.params.slug}
+
       {console.log(props)}
     </div>
   );
